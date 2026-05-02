a46c19d265cdc8b6516a78bbed00ffc7b02f4f00
drewmcarthur <drewmcarthur1@gmail.com>
2026-05-02 13:07:26 -0500

    use http

--- scripts/quartz-init.sh
+++ scripts/quartz-init.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/bash -e
 
 if [[ ! -d .quartz ]]; then
-    git clone git@github.com:jackyzha0/quartz.git .quartz --depth=1
+    git clone https://github.com/jackyzha0/quartz.git .quartz --depth=1
     cd .quartz
     bun install
     rm -rf content
